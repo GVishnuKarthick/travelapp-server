549828ddcad19c461c51468bce8f8c63c6f96947
Vishnu Karthick.G <gvishnukarthick@Vishnus-MacBook-Air.local>
2026-03-09 23:12:17 +0530

    Fix activities type

--- Models/DayPlan.cs
+++ Models/DayPlan.cs
@@ -6,21 +6,10 @@ namespace TravelPlanApi.Models;
 public class DayPlan
 {
     public int Id { get; set; }
-
     public int Day { get; set; }
-
     public string Title { get; set; } = string.Empty;
 
-    // Stored in DB
-    public string ActivitiesJson { get; set; } = "[]";
-
-    // Used in API
-    [NotMapped]
-    public List<string> Activities
-    {
-        get => JsonSerializer.Deserialize<List<string>>(ActivitiesJson) ?? new();
-        set => ActivitiesJson = JsonSerializer.Serialize(value);
-    }
+    public List<string> Activities { get; set; } = new();
 
     public int ItineraryId { get; set; }
 
